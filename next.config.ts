46c5047646ca010991e88c5d3cde90364b3d8825
Arman Ali Khan <armaanalikhanofficial@gmail.com>
2026-03-14 20:35:16 +0000

    photo upload not working fix this error.

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.pravatar.cc',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
