e87f2fbb6c3ce9bde1a3ed823d13ede129f3dccc
Arman Ali Khan <armaanalikhanofficial@gmail.com>
2026-03-14 16:59:10 +0000

    show 2 column grid on mobile view. (_for element div_)

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -20,7 +20,7 @@ export default function AdminDashboardPage() {
   return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
         {stats.map(stat => (
           <DashboardCard key={stat.title} {...stat} />
         ))}
